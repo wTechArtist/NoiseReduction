0fdcd595437ebd1648e4e8b8bb366279c4632b4f
weiweiliang <weiweiliang@corp.netease.com>
2025-06-24 19:47:21 +0800

    add depth to normal

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
 from .NoiseReduction import *
+from .DepthToNormals import *
 
 # 注册节点映射
 NODE_CLASS_MAPPINGS = {
-    "NoiseReduction": NoiseReduction
+    "NoiseReduction": NoiseReduction,
+    "DepthToNormals": DepthToNormals
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "NoiseReduction": "Reduce Noise Filter"
+    "NoiseReduction": "Reduce Noise Filter",
+    "DepthToNormals": "VVL Depth to Normals"
 }
